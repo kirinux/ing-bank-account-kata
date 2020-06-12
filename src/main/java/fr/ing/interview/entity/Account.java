6f1ab3325e8dedf156e560aaf470ca307c0b2a29
Gérard Duong <gerard.duong@consultants-solutec.fr>
2020-06-12 14:23:21 +0200

    Correction d'erreurs de réflexion par manque des bons constructeurs.

--- src/main/java/fr/ing/interview/entity/Account.java
+++ src/main/java/fr/ing/interview/entity/Account.java
@@ -1,6 +1,9 @@
 package fr.ing.interview.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,9 @@ import java.math.BigDecimal;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
 
     @Id
