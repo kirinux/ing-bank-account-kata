83671673082333e6a688634cc6c60be0a4c641de
Gérard Duong <gerard.duong@consultants-solutec.fr>
2020-06-12 10:59:41 +0200

    Mise en place du socle applicatif spring boot.

---
+++ src/test/java/fr/ing/interview/BankAccountApplicationITest.java
@@ -0,0 +1,13 @@
+package fr.ing.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationITest {
+
+    @Test
+    void run() {
+    }
+
+}
