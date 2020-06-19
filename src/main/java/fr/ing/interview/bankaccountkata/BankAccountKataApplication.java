7c18a32dc93268efcb3302c4135654c729caa793
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-19 15:59:39 +0200

    init spring application

---
+++ src/main/java/fr/ing/interview/bankaccountkata/BankAccountKataApplication.java
@@ -0,0 +1,13 @@
+package fr.ing.interview.bankaccountkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ing/interview/bankaccountkata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ing.interview.bankaccountkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
