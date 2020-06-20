97a2f8d5d50401f541d06cae3fa1af776332ba4e
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-20 10:25:34 +0200

    creation test case for the third  use case -> show balance

--- src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
+++ src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
@@ -1,6 +1,7 @@
 package fr.ing.interview.bankaccountkata.service;
 
 import fr.ing.interview.bankaccountkata.model.Account;
+import fr.ing.interview.bankaccountkata.model.Customer;
 import fr.ing.interview.bankaccountkata.model.Transaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.test.context.event.annotation.BeforeTestClass;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -70,6 +74,26 @@ class AccountServiceImplTest {
         assertThatThrownBy(()->{ accountService.withdraw(100,account);}).isInstanceOf(Exception.class);
 
     }
+
+
+    @Test
+    void should_get_balance() throws Exception {
+        AccountService  accountServices = new AccountServiceImpl();
+        long customerId = 1L;
+        Account account1 = new Account();
+        account1.setIdAccount(1);
+        account1.setBalance(20.0);
+        Account account2 = new Account();
+        account2.setBalance(30.0);
+        account2.setIdAccount(2);
+        List<Account> accounts = Arrays.asList(account1,account2);
+        Customer customer = new Customer(1,"customer1@gmail.com",accounts);
+        double balance = accountServices.getBalance(customer,1);
+        assertEquals(balance, 20.0);
+    }
+
+
+
 
 
 }
