4f9ba616a95b57b4659109a8913e5a9d99613d97
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-20 10:35:58 +0200

    creation test case for the third  use case4 -> show transaction history

--- src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
+++ src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
@@ -56,13 +56,11 @@ class AccountServiceImplTest {
         AccountService  accountServices = new AccountServiceImpl();
         accountServices.withdraw(50.0,account);
         assertEquals(account.getTransactions().get(0).getAmount(),50.0 );
-
     }
 
 
     @Test
     void shouldRiseExceptionIfNullAccountWhenDepositOverdraft() throws Exception {
-
         assertThatThrownBy(()->{ accountService.withdraw(200,null);}).isInstanceOf(Exception.class);
     }
 
@@ -70,16 +68,14 @@ class AccountServiceImplTest {
     @Test
     void shouldWithdrawMoneyFromAccount() {
         Account account = new Account(1, 100.0, null,new ArrayList<>(), 0.0, null);
-
         assertThatThrownBy(()->{ accountService.withdraw(100,account);}).isInstanceOf(Exception.class);
 
     }
 
 
     @Test
-    void should_get_balance() throws Exception {
+    void shouldGetBalance() throws Exception {
         AccountService  accountServices = new AccountServiceImpl();
-        long customerId = 1L;
         Account account1 = new Account();
         account1.setIdAccount(1);
         account1.setBalance(20.0);
@@ -90,6 +86,23 @@ class AccountServiceImplTest {
         Customer customer = new Customer(1,"customer1@gmail.com",accounts);
         double balance = accountServices.getBalance(customer,1);
         assertEquals(balance, 20.0);
+    }
+
+    @Test
+    void shouldGetTransactions() throws Exception {
+        AccountService  accountServices = new AccountServiceImpl();
+        Account account = new Account(1, 100.0, new ArrayList<Transaction>());
+        accountServices.deposit(100,account);
+        accountServices.withdraw(80,account);
+        accountServices.deposit(20,account);
+        accountServices.withdraw(50,account);
+        Account account2 = new Account();
+        account2.setBalance(30.0);
+        account2.setIdAccount(2);
+        List<Account> accounts = Arrays.asList(account,account2);
+        Customer customer = new Customer(1,"customer1@gmail.com",accounts);
+        List<Transaction> transactions = accountServices.getTransactions(customer,1);
+        assertEquals(transactions.size(), 4);
     }
 
 
