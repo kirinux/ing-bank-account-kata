888d51304858cd0197267287b499178136f42a82
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-20 08:54:33 +0200

    implementation of the second user story withdraw money

--- src/main/java/fr/ing/interview/bankaccountkata/service/AccountServiceImpl.java
+++ src/main/java/fr/ing/interview/bankaccountkata/service/AccountServiceImpl.java
@@ -1,48 +1,58 @@
 package fr.ing.interview.bankaccountkata.service;
 
 import fr.ing.interview.bankaccountkata.model.Account;
+import fr.ing.interview.bankaccountkata.model.Transaction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
+@Service
+@Transactional
 public class AccountServiceImpl implements AccountService{
 
     public static final double AMOUNT_MIN = 0.01;
-    public TransactionServiceImpl transactionService;
+    private TransactionServiceImpl transactionService;
 
 
 
     @Override
-    public double deposit(double amount, Account account) throws Exception{
+    public Account deposit(double amount, Account account) throws Exception{
+        if(account == null){
+            throw new RuntimeException ("account is null");
+        }
+
         if(amount < AMOUNT_MIN){
-            throw new Exception("invalide amount");
+            throw new Exception();
         }
 
         account.setBalance((account.getBalance()+amount));
-        transactionService.addTransaction();
-
-            return account.getBalance() ;
-
+        transactionService.addTransaction(new Transaction(1L, LocalDate.now(), amount));
+        return account ;
     }
 
     @Override
-    public long withdraw() {
-        return 0;
+    public Account withdraw(double amount, Account account) throws Exception {
+
+        if(account == null){
+            throw new RuntimeException ("account is null");
+        }
+
+        if((account.getBalance() - amount) < 0 ){
+            throw new RuntimeException ("you don't have enough money");
+        }
+
+
+        account.setBalance((account.getBalance() - amount));
+        transactionService.addTransaction(new Transaction(1L, LocalDate.now(), amount));
+
+        return account ;
     }
 
-    @Override
-    public long getBalance() {
-        return 0;
-    }
 
-    @Override
-    public void addTransactionHistory() {
 
-    }
-
-    @Override
-    public void getTransactionList() {
-
-    }
 
 
 }
