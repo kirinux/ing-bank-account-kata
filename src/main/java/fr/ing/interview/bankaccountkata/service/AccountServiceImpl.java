cf5db2b2c84303d18d2cca7d55f9c7bc321422a4
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-20 10:39:15 +0200

    implementation of the third and fourth user story show balance and transaction history

--- src/main/java/fr/ing/interview/bankaccountkata/service/AccountServiceImpl.java
+++ src/main/java/fr/ing/interview/bankaccountkata/service/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package fr.ing.interview.bankaccountkata.service;
 
 import fr.ing.interview.bankaccountkata.model.Account;
+import fr.ing.interview.bankaccountkata.model.Customer;
 import fr.ing.interview.bankaccountkata.model.Transaction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 public class AccountServiceImpl implements AccountService{
@@ -31,7 +33,7 @@ public class AccountServiceImpl implements AccountService{
 
         Transaction t  = new Transaction(2L, LocalDate.now(), amount,account);
         account.getTransactions().add(t);
-      //  transactionService.addTransaction( t ,account);
+
 
         return account ;
     }
@@ -60,17 +62,16 @@ public class AccountServiceImpl implements AccountService{
 
 
     @Override
-    public double getBalance(int idAccount) {
-        return getAccount(idAccount).getBalance() ;
+    public double getBalance(Customer customer, int idAccount) throws Exception {
+        Optional<Account> account =  customer.getAccounts().stream().filter(a->a.getIdAccount() == idAccount).findFirst() ;
+        if(account.isPresent()){return account.get().getBalance();}else{throw  new Exception();}
+
+
     }
 
 
-
-    @Override
-    public void getTransactionList() {
-
+    public List<Transaction> getTransactions(Customer customer,long idAccount) {
+        return customer.getAccounts().stream().filter(a->a.getIdAccount()==idAccount).findFirst().get().getTransactions();
     }
-    Account getAccount(int idAccount){ return null;}
-    List<Account> getListAccounts(){ return null;}
 
 }
