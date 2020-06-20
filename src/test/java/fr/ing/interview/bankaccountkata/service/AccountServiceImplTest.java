9d9bb239de01de8c1c42a6792831c4f389f09ec1
@abdelhafid.kahoul <abdelhafid.kahoul@etu.u-pec.fr>
2020-06-20 08:46:58 +0200

    creation test case for the second use case -> withdraw

--- src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
+++ src/test/java/fr/ing/interview/bankaccountkata/service/AccountServiceImplTest.java
@@ -1,66 +1,73 @@
 package fr.ing.interview.bankaccountkata.service;
 
 import fr.ing.interview.bankaccountkata.model.Account;
-import fr.ing.interview.bankaccountkata.model.Customer;
-import fr.ing.interview.bankaccountkata.model.Transaction;
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 
-
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 
 class AccountServiceImplTest {
 
-
+    @Autowired
     AccountService accountService;
-    Account account;
+
 
 
 
     @BeforeTestClass
     public void init(){
         accountService = new AccountServiceImpl();
-        account = new Account(1, 100.0, null,null, 100.0, null)   ;
-    }
+     }
 
 
 
     @Test
     void shouldUpdateAmountWhenDeposit() throws Exception {
-        accountService.deposit(100,account);
-        assertEquals(account.getBalance(),account.getBalance() + 100 );
+        Account account = new Account(1, 100.0, null,null, 100.0, null);
+        AccountService  accountServices = new AccountServiceImpl();
+        accountServices.deposit(100,account);
+        assertEquals(account.getBalance(),200.0 );
     }
 
 
     @Test
     void shouldRiseExceptionWhenDeposit() throws Exception {
+        Account account = new Account(1, 100.0, null,null, 100.0, null);
         assertThatThrownBy(()->{ accountService.deposit(100,account);}).isInstanceOf(Exception.class);
     }
 
-
     @Test
-    void withdraw() {
+    void shouldSaveTransactionWhenDeposit() throws Exception {
+        Account account = new Account(1, 100.0, null,new ArrayList<>(), 100.0, null);
+        accountService.withdraw(200,null);
+        assertEquals(account.getTransactions().get(0).getAmount(),50.0 );
+
     }
 
-    @Test
-    void getBalance() {
-    }
 
     @Test
-    void addTransactionHistory() {
+    void shouldRiseExceptionIfNullAccountWhenDepositOverdraft() throws Exception {
+
+        assertThatThrownBy(()->{ accountService.withdraw(200,null);}).isInstanceOf(Exception.class);
     }
 
+
     @Test
-    void getTransactionList() {
+    void shouldWithdrawMoneyFromAccount() {
+        Account account = new Account(1, 100.0, null,new ArrayList<>(), 0.0, null);
+
+        assertThatThrownBy(()->{ accountService.withdraw(100,account);}).isInstanceOf(Exception.class);
+
     }
+
 
 }
